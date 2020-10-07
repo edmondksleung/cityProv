e47b9a8df3355a8a187f4c84f7c8deaea6c7b52a
Edmond Leung <edmondksleung@hotmail.com>
2020-10-06 23:37:03 -0700

    added connection

--- Startup.cs
+++ Startup.cs
@@ -27,9 +27,9 @@ namespace CityProv
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(
-            //        Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDbContext<CityProvContext>(options =>
                 options.UseSqlServer(
